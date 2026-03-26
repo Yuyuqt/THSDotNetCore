93ee23f4aad9f045c62a98a19db1d21aac9f004c
Yuyuqt <aiyurina1610@gmail.com>
2026-03-26 16:35:14 +0630

    feat: minimal API

---
+++ THSDotNetCoreConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+
+var blog = new BlogModel
+{
+    Id = 1,
+    Title = "Test title",
+    Author = "Test author",
+    Content = "Test content",
+
+};
+
+//string jsonStr = JsonConvert.SerializeObject(blog, Formatting.Indented);
+string jsonStr = blog.ToJson(); //#C to JSON
+
+Console.WriteLine(jsonStr);
+
+string jsonStr2 = """{ "Id": 1, "Title": "Test title","Author": "Test author","Content": "Test content", "DeleteFlag": false}""";
+ var blog2 = JsonConvert.DeserializeObject<BlogModel>(jsonStr2);    
+
+//System.Text.Json.JsonSerializer.Serialize(blog);
+//System.Text.Json.JsonSerializer.Deserialize<BlogModel>(jsonStr2);
+  
+Console.WriteLine(blog2.Title);  
+Console.ReadLine();
+
+public class BlogModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Content { get; set; }
+    public bool DeleteFlag { get; set; }
+}
+
+public static class Extensions //DevCode
+{ 
+    public static string ToJson(this object obj)
+    {
+        string jsonStr = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        return jsonStr;
+    }
+}
+
---
+++ THSDotNetCoreMinimalAPI/EndPoints/Blog/BlogEndpoint.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using THSDotNetCore.Database.Models;
+
+namespace THSDotNetCoreMinimalAPI.EndPoints.Blog
+{
+    public static class BlogEndpoint
+    {
+        //public static string Test(this string i)
+        //{
+        //    return i.ToString();
+        //}
+
+        public static void MapBlogEndpoint(this IEndpointRouteBuilder app)
+        {
+
+            app.MapGet("/blogs", () =>
+            {
+                AppDbContext db = new AppDbContext();
+                var model = db.TblBlogs.AsNoTracking().ToList();
+                return Results.Ok(model);
+            })
+                .WithName("GetBlogs")
+            .WithOpenApi();
+
+
+            app.MapGet("/blogs/{id}", (int id) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs
+                .AsNoTracking()
+                .FirstOrDefault(x => x.BlogId == id);
+                if (item == null)
+                {
+                    return Results.BadRequest("No data found");
+                }
+
+                return Results.Ok(item);
+            })
+                .WithName("GetBlog")
+            .WithOpenApi();
+
+
+
+            app.MapPost("/blogs", (TblBlog blog) =>
+            {
+                AppDbContext db = new AppDbContext();
+                db.TblBlogs.Add(blog);
+                db.SaveChanges();
+                return Results.Ok(blog);
+            })
+            .WithName("CreateBlogs")
+            .WithOpenApi();
+
+
+
+            app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs
+                .AsNoTracking()
+                .FirstOrDefault(x => x.BlogId == id);
+                if (item == null)
+                {
+                    return Results.BadRequest("No data found");
+                }
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return Results.Ok(blog);
+            })
+            .WithName("UpdateBlog")
+            .WithOpenApi();
+
+
+            app.MapDelete("/blogs/{id}", (int id) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs
+                .AsNoTracking()
+                .FirstOrDefault(x => x.BlogId == id);
+                if (item == null)
+                {
+                    return Results.BadRequest("No data found");
+                }
+
+                db.Entry(item).State = EntityState.Deleted;
+
+
+                db.SaveChanges();
+                return Results.Ok();
+            })
+                .WithName("DeleteBlogs")
+            .WithOpenApi();
+        }
+    }
+}
